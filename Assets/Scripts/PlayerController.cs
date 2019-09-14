bc996bbbeed710b247802489cc556eb47f41a047
Nickvillano <32552845+Nickvillano@users.noreply.github.com>
2019-09-14 15:44:59 -0400

    Changed Player colors, Camera Script, Added walls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
-
+    public float speed;
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -18,7 +18,7 @@ public class PlayerController : MonoBehaviour
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        rb.AddForce(movement);
+        rb.AddForce(movement * speed);
         
     }
 }
